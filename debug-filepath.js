a3148e64b0964a1655a2d395e4bff305f47d8809
GEEKIAM <afedulov.geekiam@gmail.com>
2016-12-02 01:51:11 +0200

    Handle creation w/o passing namespace

--- debug-filepath.js
+++ debug-filepath.js
@@ -5,11 +5,16 @@ const appRoot = require('app-root-path');
 
 function debugWrapper (namespace) {
   const creatorFilepath = formatPath(stackTrace.get()[1].getFileName());
-  return debug(`${namespace}:${creatorFilepath}`);
+
+  return debug(`${namespace || ''}${namespace ? '::' : ''}${creatorFilepath}`);
 }
 
 function formatPath (filepath) {
-  return filepath.replace(appRoot, '').split(path.sep).join(':');
+  filepath = filepath.replace(appRoot, '');
+  filepath = filepath.charAt(0) === path.sep ? filepath.substr(1) : filepath;
+  filepath = filepath.split(path.sep).join(':');
+
+  return filepath;
 }
 
 module.exports = debugWrapper;
